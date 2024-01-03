d52145c8bb520d2a6f0e017f4609cf5f2cb2dba9
2jin2.1031 <2jin2.1031@gmail.com>
2024-01-03 15:08:57 +0900

    feat: create notice DTOs

--- src/main/java/com/halo/eventer/dto/notice/NoticeResDto.java
+++ src/main/java/com/halo/eventer/dto/notice/NoticeResDto.java
@@ -1,2 +1,26 @@
-package com.halo.eventer.dto.notice;public class NoticeResDto {
+package com.halo.eventer.dto.notice;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Schema(name = "공지사항 정보")
+public class NoticeResDto {
+
+    @Schema(example = "주류 안내")
+    private String title;
+
+    @Schema(example = "모든 주류는 직접 구입해 주셔야 합니다.")
+    private String content;
+
+    @Schema(example = "2024-01-03T23:00:00")
+    private LocalDateTime useTime;
+
 }
+
